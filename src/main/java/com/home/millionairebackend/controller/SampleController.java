6e961b5c3e17f7ccd05a47f35d7023cf85dea49a
leesanghooooon <sanghoooon0726@gmail.com>
2022-01-27 16:22:07 +0900

    1.jasypt 설정관련 gradle 추가 (현재 미사용)

--- src/main/java/com/home/millionairebackend/controller/SampleController.java
+++ src/main/java/com/home/millionairebackend/controller/SampleController.java
@@ -1,12 +1,15 @@
 package com.home.millionairebackend.controller;
 
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class SampleController {
@@ -16,6 +19,13 @@ public class SampleController {
     @ApiOperation(value = "TEST API", notes = "TEST API입니다.")
     @GetMapping
     public ResponseEntity<?> apiTest() {
+
+        StandardPBEStringEncryptor spe = new StandardPBEStringEncryptor();
+        spe.setAlgorithm("PBEWithMD5AndDES");
+        spe.setPassword("millionaire");
+
+        log.info("millionaire === {}",spe.encrypt("millionaire"));
+
         entity = new ResponseEntity<>("SUCESS", HttpStatus.OK);
         return entity;
     }
